a0969013ab378dadcbfbc588e692cff31d63d5a9
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-08 19:38:22 -0500

    add button create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 
 // importamos los componentes
 import CompShowBlogs from './blog/ShowBlogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <CompShowBlogs/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/blog/ShowBlogs.js
+++ frontend/src/blog/ShowBlogs.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 
-const URI = 'http:://localhost:8000/blogs/'
+const URI = 'http://localhost:8000/blogs/'
 
 const CompShowBlogs = () => {
     const [blogs, setBlog] = useState([])
@@ -26,6 +26,7 @@ const CompShowBlogs = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col'>
+                    <Link to="/create" className='btn btn-primary mt-2 mb-2'>Crear</Link>
                     <table className='table'>
                         <thead className='table-primary'>
                             <tr>
@@ -37,7 +38,7 @@ const CompShowBlogs = () => {
                         <tbody>
                             {blogs.map (( blog) => (
                                 <tr key={blog.id}>
-                                    <td>{ blog.title}</td>
+                                    <td>{blog.title}</td>
                                     <td>{blog.content}</td>
                                     <td>
                                         <Link to={`/edit/${blog.id}`} className='btn btn-info'>Edit</Link>
